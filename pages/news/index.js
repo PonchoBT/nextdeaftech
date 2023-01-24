3f3e78b29c0e551020b6c86e6dc454b658c9b03d
Alfonso BT <ponchobt.dev@gmail.com>
2023-01-24 12:47:19 -0600

    Add url Api

---
+++ pages/news/index.js
@@ -0,0 +1,24 @@
+import Layout from "../../components/Layout"
+
+export default function News({ results }) {
+    return(
+      <Layout>
+        <h1>Top Stories</h1>
+      </Layout>
+    )
+}
+
+const API_KEY = "s0ejBTwuLw12utUNn8nAkfAACzMTbWej"
+export async function getStaticProps() {
+  const URL = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`
+  const response = await fetch(URL)
+  const data = await response.json()
+  console.log(data.results)
+  // The value of the `props` key will be
+  //  passed to the `Home` component
+  return {
+    props: {
+      results : data.results
+    }
+  }
+}
