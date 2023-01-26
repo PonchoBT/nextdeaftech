a29ed6c3969eec94ce97753f0573712125efa97d
Alfonso BT <ponchobt.dev@gmail.com>
2023-01-26 11:09:12 -0600

    Add menu

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,49 @@
+// import Link from "next/link"
+// import styles from '../styles/Home.module.css'
+
+// /* Menu Component here */
+// function Menu() {
+//     return(
+//         <ul>
+//             <Link href="/">Home</Link>&nbsp;
+//             <Link href="/contacts">Contacts</Link>
+//         </ul>
+//     )
+// }
+
+// export default function Layout({children }) {
+//     return(
+//         <div className={styles.container}>
+//             {/* insert Menu here */}
+//             <Menu />
+//             {children}
+//         </div>)
+// }
+
+
 import Link from "next/link"
-import styles from '../styles/Home.module.css'
 
-/* Menu Component here */
-function Menu() {
+export default function Layout({ children }) {
+    const links = [
+        {
+            title: "Home", 
+            path: "/"
+        }, 
+        {
+            title: "Top Stories", 
+            path: "/news/top-stories"
+        }, 
+        {
+            title: "Popular", 
+            path: "/news/popular"
+        }
+      ];
     return(
-        <ul>
-            <Link href="/">Home</Link>&nbsp;
-            <Link href="/contacts">Contacts</Link>
-        </ul>
+        <>
+         {links.map(link => {
+            return <Link href={link.path}>{link.title}{" "}</Link>;
+         })}
+          {children}
+        </>
     )
-}
-
-export default function Layout({children }) {
-    return(
-        <div className={styles.container}>
-            {/* insert Menu here */}
-            <Menu />
-            {children}
-        </div>)
 }
